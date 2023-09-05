25284dd9f3c0752b690b68603b66f9eb2e827f42
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-09-05 14:45:35 +0300

    add some exercises

--- JavaScript/35 Callbacks/exercise.js
+++ JavaScript/35 Callbacks/exercise.js
@@ -1,1 +1,10 @@
+function repeatHello(callback) {
+  const repeat = setInterval(callback, 1000);
+
+  setTimeout(() => clearInterval(repeat), 10000)
+}
+
+repeatHello(() => {
+  console.log("hello");
+});
 
--- JavaScript/36 Callbacks/exercise.js
+++ JavaScript/36 Callbacks/exercise.js
@@ -1,1 +1,8 @@
+const repeatHello = (callback) => {
+  const interval = setInterval(callback, 500);
+  setTimeout(() => clearInterval(interval), 5000);
+};
 
+repeatHello(() => {
+  console.log("Hello");
+});
