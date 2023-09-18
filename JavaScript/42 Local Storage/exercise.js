59b4f0ebdb6da0946bd032da14a953d9afd1c8e1
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-09-19 00:01:00 +0300

    all exercises have done

--- JavaScript/42 Local Storage/exercise.js
+++ JavaScript/42 Local Storage/exercise.js
@@ -1,5 +1,7 @@
 const user = {
-  id: 1,
-  name: "John",
-  age: 25,
+	id: 1,
+	name: "John",
+	age: 25,
 };
+
+localStorage.setItem("user", JSON.stringify(user));
--- JavaScript/43 Local Storage/exercise.js
+++ JavaScript/43 Local Storage/exercise.js
@@ -1,5 +1,11 @@
 const user = {
-  id: 1,
-  name: "John",
-  age: 25,
+	id: 1,
+	name: "John",
+	age: 25,
 };
+
+localStorage.setItem("user", JSON.stringify(user));
+
+const localData = JSON.parse(localStorage.getItem("user"));
+
+console.log(localData);
