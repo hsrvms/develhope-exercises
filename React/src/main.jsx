ad2954152da5e749031ce634620d011cb8c51fc1
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-05 13:42:39 +0300

    09-components

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+//! 09 - Components
 
 const root = createRoot(document.getElementById("root"));
 
