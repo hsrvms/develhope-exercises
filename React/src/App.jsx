cffc1580d09c7b6070c14b871fa3dd179eb1dd4c
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-05 12:34:13 +0300

    01-jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,16 @@
 const App = () => {
-  const hello = 'Hello World!';
+	//! 01 - JSX 
+	const hello = "Hello World";
 
-  function sayHello(name) {
-    return `<h1>Hello, ${name}</h1>`
-  }
-
-  return <h1>{sayHello('Husrev')}</h1>
-}
+	return (
+		<>
+			<h1>{hello}</h1>
+		</>
+	);
+};
 
 export default App;
+
+
+
+
