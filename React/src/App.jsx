891c6c419db4e673c0c5172197ba60270ddab47c
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-05 12:48:07 +0300

    02-jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,6 +1,12 @@
 const App = () => {
-	//! 01 - JSX 
+	//! 01 - JSX
 	const hello = "Hello World";
+
+	//! 02 - JSX
+	//? This function returns an h1 tag according to exercises' explanation or I misunderstood it. I think we don't need to return any tags in React;
+	function sayHello(name) {
+		return `<h1>Hello, ${name}</h1>`;
+	}
 
 	return (
 		<>
@@ -10,7 +16,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
