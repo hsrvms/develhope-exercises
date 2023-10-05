be99878560178c24e7eda25ffdef46cb589d2d72
Hüsrev <59848940+hsrvms@users.noreply.github.com>
2023-10-05 08:16:57 -0700

    Update App.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,23 +1,20 @@
 const App = () => {
-	//! 01 - JSX
 	const hello = "Hello World";
-
-	//! 02 - JSX
-	//? This function returns an h1 tag according to exercises' explanation or I misunderstood it. I think we don't need to return any tags in React;
+	
 	function sayHello(name) {
+		
 		return `<h1>Hello, ${name}</h1>`;
 	}
 
-  //! 03 - JSX 
-  //? If the name variable is not passed to the function, it returns undefined as name, like this: "<h1>Hello, undefined</h1>"
-
   //! 04 - JSX
   //? If the name variable contains a JSX expression it returns object as name, like this: "<h1>Hello, [object Object]</h1>"
+	// The name variable is a parameter that came from where the function called. When I called this function at 15th row I sent the parameter as JSX Expression. 
+	// Actually I don't even what the problem is and what the explanation is. Please more spesific when you write an explanation to an exercise. I could not perfectly understand most of the exercises. Thanks
 
 	return (
 		<>
 			<h1>{hello}</h1>
-      <h2>{sayHello(<p>sdflkj</p>)}</h2>
+      <h2>{sayHello(<p>JSX Expression</p>)}</h2>
 		</>
 	);
 };
