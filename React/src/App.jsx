78e67d5bc1f66ce1e1584e7805aea6e865373059
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-14 20:42:53 +0300

    24-conditional-rendering

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
 			<Welcome name={"Husrev"} age={17}/>
 			<Welcome name={"Elrond"} age={29}/>
 			<Welcome name={"Harmony"} age={85}/>
+			<Welcome name={"John"} age={33}/>
 		</div>
 	);
 };
