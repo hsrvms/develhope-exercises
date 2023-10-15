ab04a84634e3d10a1de5b69d358b834561e46987
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-16 00:15:06 +0300

    40-handling-events

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,23 +1,30 @@
 // import { useState, useEffect } from "react";
 import "./App.css";
 
-function MouseClicker() {
-
-	// ! I can simply prevent button to print its name attribute to the console by changing from e.currentTarget.name to e.target.name;
+function MultiButton() {
+	function handleClick(e) {
+		console.log(e.target.name);
+	}
 
 	return (
-		<>
-			<button name="one" onClick={(e) => console.log(e.target.name)}>
-				<img src="https://assets2.razerzone.com/images/pnx.assets/40f75b61f5b8ce6fe765383edcd52978/razer-playstation-line-wolverine-v2-pro_500x500.jpg" width={240} height={240} onClick={(e) => console.log(e.target.src)}/>
+		<div>
+			<button name="one" onClick={handleClick}>
+				One
 			</button>
-		</>
+			<button name="two" onClick={handleClick}>
+				Two
+			</button>
+			<button name="three" onClick={handleClick}>
+				Three
+			</button>
+		</div>
 	);
 }
 
 const App = () => {
 	return (
 		<div id="app">
-			<MouseClicker />
+			<MultiButton />
 		</div>
 	);
 };
