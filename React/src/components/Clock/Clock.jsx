371c42c492b731bf26d5fe61bee38e961bfd289f
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-11-01 18:33:03 +0300

    context-3

--- React/src/components/Clock/Clock.jsx
+++ React/src/components/Clock/Clock.jsx
@@ -1,15 +1,15 @@
-import { useContext } from 'react'
+import { useContext,  } from 'react'
 import { LanguageContext } from '../../LanguageContext'
 import './Clock.css'
 
 
 const Clock = () => {
 
-  const language = useContext(LanguageContext)
+  const lang = useContext(LanguageContext)
 
   return (
     <div className="clock">
-      <div>{language === 'en' ? 'Current Time' : 'Simdiki Zaman'}</div>
+      <div>{lang === 'en' ? 'Current Time' : 'Simdiki Zaman'}</div>
       <div>Clock</div>
       <br />
     </div>
