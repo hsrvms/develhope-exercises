6833ad485e7319a33de53bad8b44b3632af43e4c
Hüsrev <59848940+hsrvms@users.noreply.github.com>
2023-11-03 06:48:34 -0700

    Update App.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,13 +4,14 @@ import "./App.css";
 function Counter() {
 	const [counter, setCounter] = useState(0);
 
-	//! setter functions' parameters should be an immediate value because they need to assign that value to the state variable which is "counter" in this case. Every time I click the button the counter should increment 1 by 1, but I dont need to call a function for this again.
+	//! setter functions' parameters should be a callback function because they need to assign the expected value using prev state value. 
+	
 	
 
 	return (
 		<div>
 			<h2>{counter}</h2>
-			<button onClick={() => setCounter(counter+1)}>Increment</button>
+			<button onClick={() => setCounter(prevCounter => prevCounter+1)}>Increment</button>
 		</div>
 	);
 }
