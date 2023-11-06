0ae423264bc61a79b6f8ddc4a593c25ed3e74a53
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-11-06 19:44:07 +0300

    custom-hooks-01

--- React/src/components/Counter.jsx
+++ React/src/components/Counter.jsx
@@ -1,8 +1,19 @@
+import { useCounter } from '../hooks/useCounter'
+
 
 const Counter = () => {
 
+  const { count, increment, decrement, reset } = useCounter(0);
+
   return (
-    <div style={{display: 'inline', backgroundColor: 'red', padding: '.5rem'}}>Counter</div>
+    <div style={{ display: 'inline', backgroundColor: 'red', padding: '.5rem' }}>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
   )
 }
 
