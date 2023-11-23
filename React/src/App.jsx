50a5ee490f71a35d91352514c70137a2d24c9876
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-11-23 17:27:26 +0300

    react-router-02

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route} from 'react-router-dom';
 
 import "./App.css";
 import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 const App = () => {
 
 	return (
 			<Routes>
 				<Route path="/" element={<Welcome />} />
+				<Route path="/counter" element={<Counter />} />
 			</Routes>
 	)
 
